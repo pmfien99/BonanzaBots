ca976bf28c2938bb9916470f1eed423c83ce3b01
Patrick Fien <pmfien99@icloud.com>
2022-04-10 23:16:41 -0400

    Bot updates

--- bzabot.js
+++ bzabot.js
@@ -8,6 +8,10 @@ const { Web3Provider } = require('@ethersproject/providers');
 const { EAFNOSUPPORT } = require('constants');
 const { workerData } = require('worker_threads');
 const { MessageAttachment, MessageEmbed } = require('discord.js');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const fs = require('node:fs');
 
 // Process ENV Variables
 const stakingAddress = process.env.STAKING_CONTRACT_ADDRESS
@@ -18,26 +22,66 @@ const UpdatesChannel = process.env.UPDATES_DISCORD_CH
 const StakingChannel = process.env.STAKING_DISCORD_CH
 const RewardsChannel = process.env.REWARDS_DISCORD_CH
 
-
-// ABI for Events 
-let abi = [ "event Transfer(address indexed from, address indexed to, uint value)" ];
-let iface = new ethers.utils.Interface(abi);
-
 // Establish Connection to Web3 BSC Provider + Contract
 const webSocketProvider = new ethers.providers.WebSocketProvider(process.env.WEB_SOCKET_NODE);
 const BZAStakeContract = new ethers.Contract(stakingAddress, stakingABI, webSocketProvider);
 const BUSDContract = new ethers.Contract(BUSDAddress, ERC20ABI, webSocketProvider);
 var BZAConnected = null;
 
+let override = {
+    gasLimit: 5000000,
+};
+
 // Setup and Establish Discord Bot Connection
 const client = new Client({
     intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
 })
+// Discord - ON BOT CONNECT 
+client.on('ready', function(e) {
+    console.log(`Logged in as ${client.user.tag}!`)
+    client.channels.cache.get(DiscordBotChannel).send('BZA Bot Connected');
+    client.channels.cache.get(DiscordBotChannel).send('Watching Contract @ ' + 'https://testnet.bscscan.com/address/' + stakingAddress);
+    dailyUpdates()
+})
+
+// Connect Discord Bot
+// Establish Commands
+// const commands = [];
+// const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+// const clientId = '960667114111389706';
+// const guildId = '863979368304345139';
+
+// for (const file of commandFiles) {
+// 	const command = require(`./commands/${file}`);
+// 	commands.push(command.data.toJSON());
+// }
+
+// const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_TOKEN);
+
+// (async () => {
+//     await client.login(process.env.DISCORD_TOKEN)
+// 	try {
+// 		console.log('Started refreshing application (/) commands.');
+
+// 		await rest.put(
+// 			Routes.applicationGuildCommands(clientId, guildId),
+// 			{ body: commands },
+// 		);
+
+// 		console.log('Successfully reloaded application (/) commands.');
+// 	} catch (error) {
+// 		console.error(error);
+// 	}
+// })();
 
 // ********************************
 // RECURRING FUNCTIONS
 //
 // ********************************
+    
+
+client.login(process.env.DISCORD_TOKEN)
 
 
 // ON BOT DEPLOY
@@ -49,18 +93,17 @@ async function establisContractConnection() {
     BZAConnected = BZAStakeContract.connect(EOA)
 
     // Set daily time inerval on reward distributions 
-    setInterval(dailyRewards, 86400001);
-    setInterval(dailyUpdates, 86400001);
+    setInterval(dailyRewards, 10000);
+    setInterval(dailyUpdates, 3600000);
 
 }
 
 // Daily dist. of rewards
 async function dailyRewards() {
-    client.channels.cache.get(DiscordBotChannel).send('Sending New Rewards Test ever 5 seconds');
-
+    client.channels.cache.get(DiscordBotChannel).send('Sending New Rewards Over TestNet Every few Minutes');
     try {
         const rewards = await BZAConnected.distributeRewards()
-        await client.channels.cache.get(DiscordBotChannel).send("Rewards Dsitributed Today!");
+        await client.channels.cache.get(DiscordBotChannel).send("Rewards Dsitributed For This Period!");
 
     } catch (e) {
         console.log("Reward Distribution Failed")
@@ -71,22 +114,14 @@ async function dailyRewards() {
 
 establisContractConnection();
 
-// Discord - ON BOT CONNECT 
-client.on('ready', function(e) {
-    console.log(`Logged in as ${client.user.tag}!`)
-    client.channels.cache.get(DiscordBotChannel).send('BZA Bot Connected');
-    
-})
-
-// Connect Discord Bot
-client.login(process.env.DISCORD_TOKEN)
 
 
 
 
 async function getBUSDBal(){
     try {
-        var balance = await BUSDContract.balanceOf("0x5548ED93f4255875346b0f71562d58026cDf0FdE");
+        var balance = await BUSDContract.balanceOf(BZAStakeContract);
+        console.log(balance)
     } catch (e) {
         console.log("fatal error");
         console.log(e);
@@ -96,40 +131,15 @@ async function getBUSDBal(){
 }
 
 async function createReward(_amount){
-    try {
+    try {        
+        const amt = ethers.utils.parseEther(_amount.toString())
         const transaction = await BZAConnected.createRewardSchedule(_amount , BUSDAddress)
-        console.log("new reward schedule created")
-        console.log(transaction)
     } catch (e) {
         console.log("fatal error");
         console.log(e);
     }
 }    
-    // uint totalAmt; 
-    //     uint dailyAmt;
-    //     uint daysRemain;  
-    //     bool active; 
 
-async function testSchedules() {
-    const rewards = await BZAConnected.getRewardSchedules()
-    for (var x = 0; x < rewards.length; x++) {
-        client.channels.cache.get(DiscordBotChannel).send("-------------------");
-        client.channels.cache.get(DiscordBotChannel).send("Reward Schedule #" + x);
-        const total = await rewards[x][0].toString()
-        const dailyAmt = await rewards[x][1].toString()
-        const daysLeft = await rewards[x][2].toString()
-        const active = await rewards[x][3].toString()
-
-
-        await client.channels.cache.get(DiscordBotChannel).send("Total Reward Amt: " + ethers.utils.formatEther(total) + "BUSD");
-        await client.channels.cache.get(DiscordBotChannel).send("Daily Distributions: " + ethers.utils.formatEther(dailyAmt)  + "BUSD");
-        await client.channels.cache.get(DiscordBotChannel).send("Days Remaining: " + daysLeft + " days");
-        await client.channels.cache.get(DiscordBotChannel).send("Reward Active: " + active);
-        client.channels.cache.get(DiscordBotChannel).send("-------------------");
-
-    }
-
-}
 
 
 // ********************************
@@ -139,7 +149,9 @@ async function testSchedules() {
 
     async function dailyUpdates() {
                 const stakes = await BZAConnected.getTotalStaked()
-                const stakeOut = await ethers.utils.formatEther(stakes)
+                const stakeOut = ethers.utils.formatEther(stakes)
+                const busd = await BUSDContract.balanceOf(stakingAddress)
+                const busdOut = ethers.utils.formatEther(busd)
                 const file = new MessageAttachment('./assets/bzaLogo.png');
     
                 const bzaReport = new MessageEmbed()
@@ -152,6 +164,7 @@ async function testSchedules() {
                 .addFields(
                 { name: '\u200B', value: '\u200B' },
                 { name: 'Current BZA Staked Balance', value: stakeOut + " BZA" },
+                { name: 'Current BUSD Contract Balance:', value: busdOut + " BUSD" },
                 { name: '\u200B', value: '\u200B' },
                 )
                 .setTimestamp()
@@ -164,11 +177,36 @@ async function testSchedules() {
 
     client.on('message',
     async function(msg){
-        if(msg.content === "test"){
+        if(msg.content === "schedules"){
+            const rewards = await BZAConnected.getRewardSchedules()
 
-            client.channels.cache.get(DiscordBotChannel).send("Current Reward Schedules on Test Net: ");
+            if (rewards.length == 0) {
+                client.channels.cache.get(DiscordBotChannel).send("Currenly No Reward Schedules");
+            } else {
+                const logo = new MessageAttachment('./assets/bzaLogo.png');
+                for (var x = 0; x < rewards.length; x++) {
+                    const total = await rewards[x][0].toString()
+                    const dailyAmt = await rewards[x][1].toString()
+                    const daysLeft = await rewards[x][2].toString()
+                    const active = await rewards[x][3].toString()
+                    const reward = new MessageEmbed()
+                    .setColor('#F19D67')
+                    .setTitle('Reward Schedule ' + x)
+                    .setAuthor({ name: 'BZA Staking Contract Watcher', iconURL: 'attachment://bzaLogo.png'})
+                    .setThumbnail('attachment://bzaLogo.png')
+                    .addFields(
+                        { name: 'Total Reward Amt: ', value: ethers.utils.formatEther(total) + " BUSD" },
+                        { name: 'Daily Distributions: ', value: ethers.utils.formatEther(dailyAmt) + " BUSD" },
+                        { name: 'Days Remaining: ', value: daysLeft },
+                        )
+                    .setTimestamp()
+            
+                    client.channels.cache.get(DiscordBotChannel).send({ embeds: [reward], files: [logo]});
+            
+            
+                }
+            }
 
-            testSchedules()
 
 
         }
@@ -186,7 +224,7 @@ const rewardFilter = {
     topics: [
         ethers.utils.id("Transfer(address,address,uint256)"),
         null,
-        hexZeroPad("0xeF4Fc808Cab6ee271538C355dB310eeC6B218490", 32)
+        hexZeroPad(stakingAddress, 32)
     ]
 };
 
@@ -197,11 +235,15 @@ const rewardFilter = {
 
 // Event Fired when BUSD is sent to the Staking Contract
 webSocketProvider.on(rewardFilter, (log) => {
-    console.log(log)
+    const data = log.data
+    const topics = log.topics 
+    let abi = [
+        "event Transfer(address indexed from, address indexed to, uint256 value)",
+      ];
+    let iface = new ethers.utils.Interface(abi);
     client.channels.cache.get(DiscordBotChannel).send("New BUSD Rewards Recieved");
-    getBUSDBal(stakingAddress);
-    createReward(amount) 
-
+    const resultData = iface.decodeEventLog("Transfer", data, topics);
+    createReward(resultData.value) 
 })
 
 // Event Fired when Reward Schedule has been Created
@@ -242,5 +284,25 @@ BZAStakeContract.on('NewStake', (staker, amt) => {
             client.channels.cache.get(StakingChannel).send("@everyone Testnet Updates");
 
 })
+
+// Event Fired when Usntake Happens
+BZAStakeContract.on('Unstake', (staker, amt) => {
+    const logo = new MessageAttachment('./assets/bzaLogo.png');
+    const stakeAlert = new MessageEmbed()
+	        .setColor('#F19D67')
+	        .setTitle('UNSTAKE on Bonanza.Money')
+	        .setAuthor({ name: 'BZA Staking Contract Watcher', iconURL: 'attachment://bzaLogo.png'})
+	        .setThumbnail('attachment://bzaLogo.png')
+	        .addFields(
+                { name: 'Unstake Value:', value: "-" + ethers.utils.formatEther(amt) + " BZA" },
+                { name: 'Staker:', value: staker },
+                )
+	        .setTimestamp()
+
+            client.channels.cache.get(StakingChannel).send({ embeds: [stakeAlert], files: [logo]});
+            client.channels.cache.get(StakingChannel).send("@everyone Testnet Updates");
+
+})
+
 
 
